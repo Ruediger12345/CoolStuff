a7735f3d0b4d49834e8583b1e098715093ba64f0
Ruediger1234 <89731983+Ruediger1234@users.noreply.github.com>
2021-08-29 18:41:09 +0200

    New Stuff
    Add stuff

---
+++ desktop/src/de/buttercat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.buttercat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.buttercat.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
